9bdbc250a499c593167a62c35142dc0f606b322f
nader <nadnonn12@gmail.com>
2025-03-18 20:11:56 -0400

    added two testing functions - still need adjustments

--- Horse AppTests/Horse_AppTests.swift
+++ Horse AppTests/Horse_AppTests.swift
@@ -1,17 +1,25 @@
-//
-//  Horse_AppTests.swift
-//  Horse AppTests
-//
-//  Created by Monika Paudel on 2025-01-28.
-//
-
 import Testing
+import XCTest
 @testable import Horse_App
 
 struct Horse_AppTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+    // unit testing to check that the functions work correct, still incorrect
+    func testAddingHorse() {
+        let initialCount = horseData.horses.count
+        horseData.addHorse(name: "Test Horse", breed: "Test Breed", time: 4)
+
+        XCTAssertEqual(horseData.horses.count, initialCount + 1, "Horse should be added to the list")
+        XCTAssertEqual(horseData.horses.last?.name, "Test Horse", "Horse name should match")
+        }
+
+    func testDeletingHorse() {
+        horseData.addHorse(name: "Test Horse", breed: "Test Breed", time: 4)
+        let initialCount = horseData.horses.count
+
+        horseData.deleteHorse(at: IndexSet(integer: initialCount - 1))
+
+        XCTAssertEqual(horseData.horses.count, initialCount - 1, "Horse should be deleted")
+        }
 
 }
